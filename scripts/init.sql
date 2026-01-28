3976674243a1827cb2bbe4eee2e43815c1f1b1c4
Amlan Pattanayak <51921355+amlan2121@users.noreply.github.com>
2026-01-28 20:45:55 +0530

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,1 +1,14 @@
 
+use master
+
+create database datawarehouse
+
+use datawarehouse
+create schema bronze
+
+create schema silver
+
+create schema gold
+
+
+-- we are creating a new data base and adding different layer of schemas
